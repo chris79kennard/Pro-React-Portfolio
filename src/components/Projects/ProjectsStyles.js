7fa09128a441b0f6bb318be92d81b6920ab4dfb8
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2022-04-01 15:10:36 -0500

    changed red buttons on project cards in projectstyles js from red to lienear gradient blue purple.

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -96,12 +96,12 @@ export const ExternalLinks = styled.a`
 color:#d4c0c0;
 font-size: 1.6rem;
 padding:1rem 1.5rem;
-background: #6b3030;
+background:linear-gradient(270deg,#13ADC7 0%,#945DD6 100%);
+opacity: 1;
 border-radius: 15px;
 transition: 0.5s;
 &:hover{
-  background: #801414;
-
+  background: linear-gradient(270deg,#13ADC7 50%,#945DD6 80%);
 }
 `;
 
